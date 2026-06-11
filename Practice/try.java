d69896194801ae156ba73aa54ed7717bdad4c6d4
Abhijeet Gorale <abhigorale99@gmail.com>
2026-06-12 00:15:09 +0530

    Add files via upload

---
+++ Practice/try.java
@@ -0,0 +1,17 @@
+class java
+{
+    public static void main(String A[])
+    {
+        int a[] ={1,2,3,4};
+        int b[]={3,4,5,6};
+
+        if(a == b)
+        {
+            System.out.println("its same value");
+        }
+        else
+        {
+            System.out.println("unique numbers are :"+a+b);
+        }
+    }
+}
